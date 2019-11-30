eee81e7c36d24787974c40776665c259b53db80e
vinod kumar <vinod.kumar@laitkor.com>
2019-11-30 15:48:36 +0530

    stack column chart

--- src/views/AuditPage/AuditReport.jsx
+++ src/views/AuditPage/AuditReport.jsx
@@ -37,13 +37,14 @@ class AuditReport extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props)
     let newCurrDate = new Date()
-    newCurrDate.setMinutes(newCurrDate.getMinutes() - 15*24*60)
+    newCurrDate.setMinutes(newCurrDate.getMinutes() - 698 *24*60)
     this.state = {
       showChat: false,
       auditInfo: [],
       hospitalFilter: [],
       modalityFilter: [],
       categoryFilter: [],
+      auditfilters: {},
       startDate: newCurrDate,
       endDate: new Date()
     }
@@ -54,8 +55,17 @@ class AuditReport extends React.Component<Props, State> {
   }
   
   getAuditInfo = () => {
-    const {startDate, endDate} = this.state
-    let formData = {from: startDate, to:endDate, hospital: this.state.hospitalFilter, modality: this.state.modalityFilter, category: this.state.categoryFilter}
+    const {startDate, endDate, hospitalFilter, modalityFilter, categoryFilter} = this.state
+    let tmpHospitalArr = (hospitalFilter) ? hospitalFilter.map( s => s.value ) : [];
+    let tmpModalityArr = (modalityFilter) ? modalityFilter.map( s => s.value ) : [];
+    let tmpCategoryArr = (categoryFilter) ? categoryFilter.map( s => s.value ) : [];
+    let formData = {
+      from: dateformat(startDate, 'dd-mm-yyyy'), 
+      to:dateformat(endDate, 'dd-mm-yyyy'), 
+      hospital: tmpHospitalArr, 
+      modality: tmpModalityArr, 
+      category: tmpCategoryArr
+    }
     this.props.getAuditInfo(formData)
   }
 
@@ -69,24 +79,41 @@ class AuditReport extends React.Component<Props, State> {
     }
   }
 
-  stackChart = (graphData: any) => {
-    // let legendArr = ['Cat 1', 'Cat 3', 'Cat 4', 'Cat 5'];
-    // {
-    //       name: 'cat 1',
-    //       data: [5, 3, 4, 7, 2]
-    //   }
-    // let catData = (graphData) ? graphData.map( s => s.Scan_Received_Date ) : [];
-    
-    // let seriesData = legendArr.map( s => ({name: s, data: []}) )
-    
-    // graphData.map( s => {
-    //   console.log('s', s)
-      
-      
-    // } )
-    // let graphRowData = (graphData) ? graphData.map( s => ({name:s.category, y: s.cat}) ) : [];
-    // let graphRowData = (graphData) ? graphData.map( s => ({name:s.category, y: s.cat}) ) : [];
+  checkLabel = (cval, seriesData)=> {
+    return (seriesData.findIndex(e => e.name === cval));
+  }
 
+  getCount = (dataSeries, dindex, cval)=> {
+    let tmpCount = 0
+    if(dataSeries && dataSeries[dindex]){
+      let countRow = dataSeries[dindex].find(e => e.cat === cval)
+      tmpCount = (countRow && countRow.count) ? countRow.count : 0
+    }
+    return tmpCount;
+  }
+
+  stackColumnChartData = (graphData: any) => {
+    const {auditfilters} = this.state
+    let legendArr = (auditfilters && auditfilters.category) ? auditfilters.category : [] ;
+    
+    let dateSeries = (graphData) ? graphData.map( s => s.Scan_Received_Date ) : [];
+    let dataSeries = (graphData) ? graphData.map( s => s.catcount ) : [];
+    let seriesData = []
+    dateSeries.forEach((dval, dindex)=> {
+      legendArr.forEach((cval, cindex)=> {
+        let tmpSeriesData = this.checkLabel(cval, seriesData)
+        if(tmpSeriesData >= 0){
+          let tmpCount = this.getCount(dataSeries, dindex, cval)
+          seriesData[tmpSeriesData].data.push(tmpCount)
+        }
+        else
+        {
+          let tmpCount = this.getCount(dataSeries, dindex, cval)
+          seriesData.push({name: cval, data: [tmpCount]})
+        }
+      })
+    })
+    return seriesData;
   }
 
   handleChange = (e: any) => {
@@ -115,62 +142,13 @@ class AuditReport extends React.Component<Props, State> {
 
   handleFilter = (e: any) => {
     e.preventDefault()
-    const {startDate, endDate, hospitalFilter, modalityFilter, categoryFilter} = this.state
-    let tmpHospitalArr = (hospitalFilter) ? hospitalFilter.map( s => s.value ) : [];
-    let tmpModalityArr = (modalityFilter) ? modalityFilter.map( s => s.value ) : [];
-    let tmpCategoryArr = (categoryFilter) ? categoryFilter.map( s => s.value ) : [];
-    let formData = {
-      from: dateformat(startDate, 'dd-mm-yyyy'), 
-      to:dateformat(endDate, 'dd-mm-yyyy'), 
-      hospital: tmpHospitalArr, 
-      modality: tmpModalityArr, 
-      category: tmpCategoryArr
-    }
-    this.props.getAuditInfo(formData)
+    this.getAuditInfo()
   }
 
-  render() {
-    const {auditInfo, hospitalFilter, modalityFilter, categoryFilter, startDate, endDate} = this.state
-    let pieData = (auditInfo.pieData) ? auditInfo.pieData.map( s => ({name:s.category, y: s.cat}) ) : [];
-    const pieOptions = {
-      chart: {
-        plotBackgroundColor: null,
-        plotBorderWidth: null,
-        plotShadow: false,
-        type: 'pie'
-      },
-      title:{text: null},
-      tooltip: {
-        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-      },
-      plotOptions: {
-        pie: {
-          allowPointSelect: true,
-          cursor: 'pointer',
-          dataLabels: {
-            enabled: true,
-            format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-          },
-          showInLegend: true
-        }
-      },
-      // legend: {
-      //   enabled: true,
-      //   layout: 'horizontal',
-      //   align: 'right',
-      //   verticalAlign: 'middle',
-      // },
-      series: [{
-        name: 'Brands',
-        colorByPoint: true,
-        data: pieData
-      }]
-    }
-    if(auditInfo.graphData){
-      this.stackChart(auditInfo.graphData)
-    }
-    // let graphData = (auditInfo.graphData) ? auditInfo.graphData.map( s => ({name:s.category, y: s.cat}) ) : [];
-    const stackOptions = {
+  stackColumnChart = (auditInfo) => {
+    const {graphData} = auditInfo
+    let dateSeries = (graphData) ? graphData.map( s => s.Scan_Received_Date ) : [];
+    return {
       chart: {
         type: 'column',
         plotBackgroundColor: null,
@@ -181,7 +159,10 @@ class AuditReport extends React.Component<Props, State> {
           text: null
       },
       xAxis: {
-          categories: ['01-01-2018', '01-02-2018', '01-03-2018', '01-04-2018', '01-05-2018']
+          categories: dateSeries,
+          scrollbar: {
+            enabled: true
+          },
       },
       yAxis: {
           min: 0,
@@ -220,26 +201,61 @@ class AuditReport extends React.Component<Props, State> {
               stacking: 'normal',
               dataLabels: {
                   enabled: true
-              }
+              },
+              cropThreshold: 10000
           }
       },
+      scrollbar: {
+        enabled: true
+      },
+      series: this.stackColumnChartData(graphData)
+    }
+  }
+
+  pieChart = (auditInfo) => {
+    let pieData = (auditInfo.pieData) ? auditInfo.pieData.map( s => ({name:s.category, y: s.cat}) ) : [];
+    return {
+      chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+      },
+      title:{text: null},
+      tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: true,
+            format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+          },
+          showInLegend: true
+        }
+      },
+      // legend: {
+      //   enabled: true,
+      //   layout: 'horizontal',
+      //   align: 'right',
+      //   verticalAlign: 'middle',
+      // },
       series: [{
-          name: 'cat 1',
-          data: [5, 3, 4, 7, 2]
-      }, {
-          name: 'cat2',
-          data: [2, 2, 3, 2, 1]
-      }, {
-          name: 'cat 3',
-          data: [3, 4, 4, 2, 5]
-      }, {
-          name: 'cat 4',
-          data: [4, 1, 3, 2, 3]
-      }, {
-          name: 'cat 5',
-          data: [1, 3, 4, 2, 5]
+        name: 'Brands',
+        colorByPoint: true,
+        data: pieData
       }]
-    };
+    }
+  }
+
+  render() {
+    const {auditInfo, hospitalFilter, modalityFilter, categoryFilter, startDate, endDate} = this.state
+    
+    const pieOptions = this.pieChart(auditInfo);
+
+    const stackOptions = this.stackColumnChart(auditInfo);
 
     const hospitalList = this.formatFilterData('hospital')
     const modalityList = this.formatFilterData('Modality')
@@ -325,7 +341,7 @@ class AuditReport extends React.Component<Props, State> {
           </div>
           <div className="col-md-6 grid-margin stretch-card">
             <div className="card">
-              <div className="card-body">
+              <div className="card-body stackChartContainer">
                 <div className="d-sm-flex align-items-center mb-4">
                   <h4 className="card-title mb-sm-0">Time-Line Audit Report</h4>
                 </div>
